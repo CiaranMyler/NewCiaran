b404a911093b359c9e0505f5eaa210a8cea33662
ciaranmyler <millermyler@gmail.comgit config --global user.email millermyler@gmail.com>
2021-02-26 12:46:30 +0000

    i made a change

---
+++ HelloWorld/src/WelcomeClass.java
@@ -0,0 +1,34 @@
+import java.awt.Point;
+import java.util.Arrays;
+import java.util.Date;
+
+public class WelcomeClass {
+
+	public static void main(String[] args) {
+	int x = 4;
+	int z = 5;
+	int y = x--;
+	
+	
+		
+		
+	System.out.println("The value of X is:" + x);
+	System.out.println("The value of y is:" + y);	
+		
+	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		 
+		
+		
+
+	}//method
+
+}//class
